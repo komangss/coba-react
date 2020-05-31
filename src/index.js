cabfad0f7cbf01739082657a59d9acb371be6acd
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-05-31 23:01:31 +0800

    first hello world

--- src/index.js
+++ src/index.js
@@ -5,18 +5,9 @@ import ReactDOM from 'react-dom'
 const element = <h1>Hello World</h1>;
 // this element will compiled by bible to :
 // React.createElement
+//  im gonna render this to real DOM (thats why we import ReactDOM)
 
 console.log(element);
-// result =
-// Object
-// $$typeof: Symbol(react.element)
-// type: "h1"
-// key: null
-// ref: null
-// props: {children: "Hello World"}
-// _owner: null
-// _store: {validated: false}
-// _self: null
-// _source: {fileName: "/var/www/html/coba-react/src/index.js", lineNumber: 5, columnNumber: 17}
-// __proto__: Object
 
+// we are gonna render it to #root in public/index.html whic thats are our container
+ReactDOM.render(element, document.getElementById('root'))
