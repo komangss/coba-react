eb358b50cc4ba79cf0bf3866d7f3f0f1f1a974a4
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-05-31 23:18:19 +0800

    install bootstrap and doing second hello world with component that i created

---
+++ src/components/counter.jsx
@@ -0,0 +1,11 @@
+// use shortcut imrc to import react Component from react
+import React, { Component } from 'react';
+
+// use shotcut cc to create class component
+class Counter extends Component {
+    render() { 
+        return ( <h1>Hello World</h1> );
+    }
+}
+ 
+export default Counter;
