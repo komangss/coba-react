34c8a278fed710da2ebb91c542f182e75563d4c2
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-06-09 00:09:54 +0800

    Lifting the state up

--- src/App.js
+++ src/App.js
@@ -3,12 +3,50 @@ import Counters from "./components/counters";
 import NavBar from "./components/NavBar";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    console.log("Button Deleted. id : ", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
         <main className="container">
-          <Counters />
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
         </main>
       </React.Fragment>
     );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,8 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
+        <a className="navbar-brand" href="localhost:3000">
+    Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
         </a>
       </nav>
     );
