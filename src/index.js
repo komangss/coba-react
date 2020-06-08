603f91c1c31eb5718fb6f878eede2d8b9326fbdd
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-06-08 23:57:17 +0800

    Multipe Components In Sync

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css";
-import Counters from './components/counters';
+import App from './App';
 
-ReactDOM.render(<Counters />, document.getElementById('root') );
+ReactDOM.render(<App />, document.getElementById('root') );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
